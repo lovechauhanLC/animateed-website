f946fe2405039ee0373cf49d9f8969c188f56ab9
lovechauhanLC <officiallove321@gmail.com>
2025-10-03 02:08:58 +0530

    optimisation in agency.jsx

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -1,85 +1,86 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import React, { useRef } from "react";
-import { ScrollTrigger } from "gsap/all";
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import { useRef } from 'react'
 
 const Agency = () => {
+
   gsap.registerPlugin(ScrollTrigger)
 
-  const imgDivRef = useRef(null)
-  const imgRef = useRef(null)
+  const imageDivRef = useRef(null)
+  const imageRef = useRef(null)
 
-  const imageArr = [
-    'https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7',
-    'https://k72.ca/images/teamMembers/Lawrence_480x640.jpg?w=480&h=640&fit=crop&s=0a878205586092164001a9afe0ef4007',
-    'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
-    'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
-    'https://k72.ca/images/teamMembers/SophieA_480x640.jpg?w=480&h=640&fit=crop&s=fcb556060c29623e706dfbc4eeca87ac',
-    'https://k72.ca/images/teamMembers/Claire_480x640.jpg?w=480&h=640&fit=crop&s=2a5f6181f85b2d13f7fb30c5555835aa',
-    'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
-    'https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6',
-    'https://k72.ca/images/teamMembers/MyleneS_480x640.jpg?w=480&h=640&fit=crop&s=55944189ac2aa83e83e2ef8125e4809e',
-    'https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a',
-    'https://k72.ca/images/teamMembers/HugoJoseph_480x640.jpg?w=480&h=640&fit=crop&s=f152025b8a59b062d1e7978b5d6544c3',
-    'https://k72.ca/images/teamMembers/MAXIME_480X640_2.jpg?w=480&h=640&fit=crop&s=fa729ef392c1761aa156d9aea9ab41e3',
-    'https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847',
-    'https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6'
+  const imageArray = [
+    'https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg',
   ]
-  
-  
-  useGSAP(function(){
-    gsap.to(imgDivRef.current,{
-      scrollTrigger:{
-        trigger:imgDivRef.current, 
-        start:"top 29.75%",
-        end:'top -125%',
-        scrub:true,
-        pin:true,
-        onUpdate: (e)=>{
-          let imgIdx = Math.floor(e.progress*imageArr.length)
-          if(imgIdx==14) imgIdx=13
-          imgRef.current.src = imageArr[imgIdx]
+
+  useGSAP(function () {
+
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start:"top 5%",
+        end:'top -160%',
+        pin: true,
+        pinSpacing: true,
+        pinReparent: true,
+        pinType: 'transform',
+        scrub: 1, // smooth scrubbing with 1s easing
+        anticipatePin: 1,
+        invalidateOnRefresh: true,
+        onUpdate: (elem) => {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(elem.progress * imageArray.length)
+          } else {
+            imageIndex = imageArray.length - 1
+          }
+          imageRef.current.src = imageArray[imageIndex]
         }
       }
     })
-
   })
 
-  return (
-    <div className="scroll-smooth">
-      <div className="section1">
-        <div ref={imgDivRef} className="h-[20vw] w-[15vw] overflow-hidden rounded-3xl absolute top-62 left-112">
-          <img
-            ref={imgRef}
-            className="h-full w-full object-cover"
-            src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
-            alt=""
-          />
-        </div>
 
-        <div className="relative font-[font2] ">
-          <div className="mt-[56vh] ">
-            <h1 className="text-[20vw] uppercase text-center leading-[17vw] ">
-              SEVEN7Y TWO
-            </h1>
+  return (
+    <div className='parent'>
+      <div id='page1' className='py-1 '>
+        <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-63 -top-80 lg:left-[30vw] left-[30vw]'>
+          <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
+        </div>
+        <div className='relative font-[font2]'>
+          <div className='lg:mt-[55vh] mt-[30vh]'>
+            <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>SEVEN7Y<br/> TWO</h1>
           </div>
-          <div className="pl-[40%] mt-20 ">
-            <p className="text-6xl">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              &nbsp;We’re inquisitive and open-minded, and we make sure
+          <div className='lg:pl-[40%] lg:mt-20 mt-4 p-3'>
+            <p className='lg:text-6xl text-xl leading-15'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure
               creativity crowds out ego from every corner. A brand is a living
               thing, with values, a personality and a story. If we ignore that,
               we can achieve short-term success, but not influence that goes the
               distance. We bring that perspective to every brand story we help
-              tell.
-            </p>
+              tell.</p>
           </div>
         </div>
       </div>
-      <div className="section2 h-screen">
+      <div id='page2' className=" h-screen">
+
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Agency;
+export default Agency
