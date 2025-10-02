d64de0110ccc71ecd11ee17280aeb52e4d22861b
lovechauhanLC <officiallove321@gmail.com>
2025-10-02 18:54:02 +0530

    home screen nav bar and full screen navbsr

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import Agency from './pages/Agency'
 import Work from './pages/Work'
+import Navbar from './components/navigations/Navbar'
+import FullScreenNav from './components/navigations/FullScreenNav'
 
 const App = () => {
   return (
     <div>
-    
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/agency' element={<Agency/>} />
-      <Route path='/work' element={<Work/>} />
-    </Routes>
+      <Navbar/>
+      <FullScreenNav/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/agency' element={<Agency/>} />
+        <Route path='/work' element={<Work/>} />
+      </Routes>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import Videos from '../components/home/Videos'
 import HomeHeroText from '../components/home/HomeHeroText'
 import HomesBottomText from '../components/home/HomesBottomText'
 
 const Home = () => {
-  return (
-    <div>
 
+
+  return (
+    <div className='text-white'>
       <div className='h-screen w-screen fixed'>
         <Videos/>
       </div>
-
-      <div className='h-screen w-screen overflow-hidden relative flex flex-col justify-between'>
+      <div className='h-screen w-screen relative pb-5 overflow-hidden flex flex-col justify-between'>
         <HomeHeroText/>
         <HomesBottomText/>
       </div>
-
     </div>
   )
 }
